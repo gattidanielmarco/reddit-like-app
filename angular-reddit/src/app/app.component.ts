519c24675a60148a364c2804b221a2856f67c31f
EC2 Default User <ec2-user@ip-172-31-95-22.ec2.internal>
2019-02-27 13:52:17 +0000

    esercizio 8.1

--- angular-reddit/src/app/app.component.ts
+++ angular-reddit/src/app/app.component.ts
@@ -1,18 +1,29 @@
 import { Component } from '@angular/core';
+import { Article } from './article/article.model'; // <-- import this
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+    sortedArticles(): Article[] {
+    return this.articles.sort((a: Article, b: Article) => b.votes - a.votes);
+  }
+  articles:Article[];   // <-- component property
+  constructor(){
+    this.articles = [
+      new Article('Angular 2', 'http://angular.io', 3),
+      new Article('Fullstack', 'http://fullstack.io', 2),
+      new Article('Angular Homepage', 'http://angular.io', 1),
+    ];
+  }
+
  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
-     let t = title.value;
-    let v= link.value;
-    console.log(`Adding article title: `+t+ ` and link: `+ v);
-    console.log(`Adding article title: `+title.value+ ` and link: `+ link.value);
     console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    this.articles.push(new Article(title.value, link.value, 0));
+    title.value = '';
+    link.value = '';
     return false;
   }
 }
-
--- angular-reddit/src/app/article/article.component.ts
+++ angular-reddit/src/app/article/article.component.ts
@@ -1,30 +1,29 @@
 import { Component, OnInit, HostBinding } from '@angular/core';
-
+import { Article } from './article.model'; //Non cancellare gli altri import
 @Component({
   selector: 'app-article', //Il tag per inserire il componente sarà <app-article>
   templateUrl: './article.component.html',
   styleUrls: ['./article.component.css']
 })
+
+
 export class ArticleComponent implements OnInit {
-  @HostBinding('attr.class') cssClass = 'card';
-  votes: number;
-  title: string;
-  link: string;
+  @HostBinding('attr.class') cssClass = 'card mt-4';
+  article:Article
 
   constructor() {
-    this.title = 'Angular 2';
-    this.link = 'http://angular.io';
-    this.votes = 10;
+    this.article = new Article('Angular 2','http://angular.io',10);
   }
 
- voteUp():Boolean { //Aggiunto tipo di ritorno
-    this.votes += 1;
-    return false; //Non propagare l'evento 
-}
-voteDown():Boolean{
-    this.votes -= 1;
-    return false; //Non propagare l'evento 
-}
+  voteUp(): Boolean {
+    this.article.voteUp(); //Modificato qui 
+    return false;
+  }
+
+  voteDown():Boolean {
+    this.article.voteDown(); //Modificato qui
+    return false;
+  }
 
   ngOnInit() {}
 }
